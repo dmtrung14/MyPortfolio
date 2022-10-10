944f5c683c8ef6648fed5a5b48aa8e6c59ea9323
dmtrung14 <dmtrung14@users.noreply.github.com>
2022-10-10 12:59:46 +0000

    Deploying to gh-pages from  @ 2323d04d1c8446b4cd68588c2a73488ac02f1be3 🚀

---
+++ precache-manifest.562dd571f090b3c70aafb8b3530d599a.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e250437717cf7f15fd65477df5be2e20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cde145affbd2e1e4e2c8",
+    "url": "/static/css/3.c07292d6.chunk.css"
+  },
+  {
+    "revision": "21f5c790cdd56bb4c82c",
+    "url": "/static/css/4.a96ca5e5.chunk.css"
+  },
+  {
+    "revision": "660dd1ce678bef46f559",
+    "url": "/static/css/main.3ab32bf8.chunk.css"
+  },
+  {
+    "revision": "029f7749d9846666e3a0",
+    "url": "/static/js/2.4aa3719c.chunk.js"
+  },
+  {
+    "revision": "b045256696e9edb485ef902b6be54c14",
+    "url": "/static/js/2.4aa3719c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cde145affbd2e1e4e2c8",
+    "url": "/static/js/3.a85befba.chunk.js"
+  },
+  {
+    "revision": "21f5c790cdd56bb4c82c",
+    "url": "/static/js/4.0513155a.chunk.js"
+  },
+  {
+    "revision": "660dd1ce678bef46f559",
+    "url": "/static/js/main.cb4dd737.chunk.js"
+  },
+  {
+    "revision": "247508e340d733a49f48",
+    "url": "/static/js/runtime-main.f1e366e1.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "6378db989c9ca679905529daa6742014",
+    "url": "/static/media/UMassLogo.6378db98.png"
+  },
+  {
+    "revision": "0430938885a271b35992bc3cbbe97ea2",
+    "url": "/static/media/bigbrothers.04309388.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "0da3f5933e51436e52d860ee8c14fe2b",
+    "url": "/static/media/fptLogo.0da3f593.png"
+  },
+  {
+    "revision": "54ff76815cac71caf4b4e631485a5b6e",
+    "url": "/static/media/ijso2019.54ff7681.png"
+  },
+  {
+    "revision": "2eab74b154757cbfb008376981db5ecf",
+    "url": "/static/media/imoLogo.2eab74b1.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "48ae19c3c20cc35a41c43d373d21bec7",
+    "url": "/static/media/mathinspiration.48ae19c3.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
